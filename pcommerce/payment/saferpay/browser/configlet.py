b01d10b0c7040c7eb5d69d014ba4c61d6df5bacb
Simon Käser <skaeser@raptus.com>
2010-07-28 14:22:07 +0000

    imported pcommerce.payment.saferpay

---
+++ pcommerce/payment/saferpay/browser/configlet.py
@@ -0,0 +1,48 @@
+from Acquisition import aq_inner
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from Products.CMFCore.utils import getToolByName
+
+from Products.CMFPlone import PloneMessageFactory as _p
+
+class Configlet(BrowserView):
+    """Saferpay configlet
+    """
+
+    errors = {}
+    values = {}
+    template = ViewPageTemplateFile('configlet.pt')
+    properties = ('account_id','test','password','bodycolor','headcolor','headlinecolor','menucolor','bodyfontcolor','headfontcolor','menufontcolor','font',)
+    required = ('account_id',)
+
+    def __call__(self):
+        self.request.set('disable_border', True)
+
+        if self.request.form.has_key('saferpay_save'):
+            self.setProperties()
+
+        props = getToolByName(self.context, 'portal_properties').saferpay_properties
+        for property in self.properties:
+            self.values[property] = props.getProperty(property, '')
+
+        return self.template()
+
+    def setProperties(self):
+        context = aq_inner(self.context)
+        utils = getToolByName(context, 'plone_utils')
+        self.errors = {}
+        for prop in self.properties:
+            self.values[prop] = self.request.form.get(prop, None)
+            if self.values[prop] == '':
+                self.values[prop] = None
+            if prop in self.required and self.values[prop] is None:
+                self.errors[prop] = _p(u'This field is required')
+        if len(self.errors) > 0:
+            utils.addPortalMessage(_p(u'Please correct the indicated errors'))
+        else:
+            props = getToolByName(context, 'portal_properties').saferpay_properties
+            for property in self.properties:
+                props._setPropValue(property, self.values[property])
+            utils.addPortalMessage(_p(u'Properties saved'))
